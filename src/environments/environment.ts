5f6c3dde2b9ba9d04589bab772c045b0fe9a94f6
Mohammad Al-Lami <mo.i.allami@gmail.com>
2025-06-19 16:17:00 -0400

    Update env variable of backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/secure-prints/',
-  //apiUrl: 'http://18.119.142.19:8080/secure-prints/',
+  //apiUrl: 'http://localhost:8080/secure-prints/',
+  apiUrl: 'http://18.119.142.19:8080/secure-prints/',
 };
